aab0b35babbe41844f5f868a715413c634936d04
Your Name <you@example.com>
2026-06-17 08:10:36 -0700

    fix: correct nvim stow nesting, remove kickstart scaffolding

--- shared/nvim/.config/nvim/init.lua
+++ shared/nvim/.config/nvim/init.lua
@@ -942,7 +942,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
@@ -980,3 +980,5 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+vim.cmd 'colorscheme industry'
